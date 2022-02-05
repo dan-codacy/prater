74429d8f06de45e33c687fc7df8cb7660ed10d7c
Dan Putman <57899154+dan-codacy@users.noreply.github.com>
2022-02-04 19:02:54 -0500

    Update .credo.exs

--- .credo.exs
+++ .credo.exs
@@ -21,14 +21,8 @@
         # You can give explicit globs or simply directories.
         # In the latter case `**/*.{ex,exs}` will be used.
         #
-        included: [
-          "lib/",
-          "src/",
-          "test/",
-          "web/",
-          "apps/"
-        ],
-        excluded: [~r"/_build/", ~r"/deps/", ~r"/node_modules/"]
+        included: ["lib/", "src/", "test/", "web/", "apps/"],
+        excluded: [~r"/_build/", ~r"/deps/"]
       },
       #
       # Load and configure plugins here:
