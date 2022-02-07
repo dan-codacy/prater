d23dbc487765640f4a5e487ad4215bec7219da9a
Dan Putman <57899154+dan-codacy@users.noreply.github.com>
2022-02-07 09:48:33 -0500

    WithClauses

--- .credo.exs
+++ .credo.exs
@@ -127,6 +127,7 @@
         {Credo.Check.Refactor.PipeChainStart,
          excluded_argument_types: [:atom, :binary, :fn, :keyword], excluded_functions: []},
         {Credo.Check.Refactor.UnlessWithElse},
+        {Credo.Check.Refactor.WithClauses, []},
 
         #
         ## Warnings
